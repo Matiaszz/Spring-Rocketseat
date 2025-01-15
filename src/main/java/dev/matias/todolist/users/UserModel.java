041a3cfd7374e9d7a799072108d3304769127ecc
Matias <alangiovanepaes@gmail.com>
2025-01-15 00:22:33 -0300

    Learning about lombok (creating getters and setters automatically

--- src/main/java/dev/matias/todolist/users/UserModel.java
+++ src/main/java/dev/matias/todolist/users/UserModel.java
@@ -1,36 +1,12 @@
 package dev.matias.todolist.users;
 
+import lombok.Data;
+// import lombok.Getter;
+// import lombok.Setter;
+
+@Data
 public class UserModel {
     private String username;
     private String name;
     private String password;
-
-    // Getters
-    //////////////////////////////////
-    public String getUsername() {
-        return username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-    //////////////////////////////////
-
-    // Settters
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
 }
--- src/main/java/dev/matias/todolist/utils/Clear.java
+++ src/main/java/dev/matias/todolist/utils/Clear.java
@@ -5,5 +5,7 @@ public class Clear {
         for (int i = 0; i < 50; i++) {
             System.out.println();
         }
+
+        System.out.println("Project Running...");
     }
 }
