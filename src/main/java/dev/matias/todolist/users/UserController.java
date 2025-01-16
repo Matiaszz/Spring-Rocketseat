394ec33fc2dcdc2ebee94f76f2666b314daaa0ad
Matias <alangiovanepaes@gmail.com>
2025-01-16 19:38:18 -0300

    Hashing password in UserModel

--- src/main/java/dev/matias/todolist/users/UserController.java
+++ src/main/java/dev/matias/todolist/users/UserController.java
@@ -1,12 +1,13 @@
 package dev.matias.todolist.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
@@ -22,9 +23,13 @@ public class UserController {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if (user != null) {
-            return ResponseEntity.status(400).body("User aleardy exists.");
-
+            return ResponseEntity.status(400).body("User Aleardy Exists");
         }
+
+        var passwordHashed = BCrypt.withDefaults().hashToString(
+                12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashed);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.ok().body(userCreated);
