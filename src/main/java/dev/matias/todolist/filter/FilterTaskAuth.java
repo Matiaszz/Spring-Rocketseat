ee81add243e8e2ba696f639983e12cf9cfd177da
Matias <alangiovanepaes@gmail.com>
2025-01-17 21:11:05 -0300

    Getting the user infos

--- src/main/java/dev/matias/todolist/filter/FilterTaskAuth.java
+++ src/main/java/dev/matias/todolist/filter/FilterTaskAuth.java
@@ -1,25 +1,50 @@
 package dev.matias.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
 
-        // execute some action
-        System.out.println("Filtering...");
-        chain.doFilter(request, response);
+        // get user and password
+
+        var authorization = request.getHeader("Authorization");
+        System.out.println("Authorization " + authorization);
+
+        // "Basic" => type of authorization
+        var authEncoded = authorization.substring("Basic".length()).trim();
+
+        // Decode, but in bytes
+        byte[] authBytes = Base64.getDecoder().decode(authEncoded);
+
+        // Transform from bytes to String (returns "username:password")
+        String authDecoded = new String(authBytes);
+
+        // split the string and put into an array (["username", "password"])
+        String[] credentials = authDecoded.split(":");
+
+        String username = credentials[0];
+        String password = credentials[1];
+
+        System.out.println(username);
+        System.out.println(password);
+        // validate user
+
+        // validate password
+
+        filterChain.doFilter(request, response);
     }
 
 }
