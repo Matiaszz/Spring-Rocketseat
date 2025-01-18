4c0ba43ea39f67631b881ffa6ab40095d99a2ee6
Matias <alangiovanepaes@gmail.com>
2025-01-18 17:18:54 -0300

    validating dates of task's json

--- src/main/java/dev/matias/todolist/task/TaskController.java
+++ src/main/java/dev/matias/todolist/task/TaskController.java
@@ -1,8 +1,10 @@
 package dev.matias.todolist.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +20,35 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         System.out.println("Controlling... ");
+
+        if (taskModel.getStartAt() == null || taskModel.getEndDate() == null) {
+            return ResponseEntity.badRequest().body("Start date and end date are required.");
+        }
+
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
+
+        LocalDateTime currentDate = LocalDateTime.now();
+
+        // Verification vaariables
+
+        Boolean startIsBefore = currentDate.isAfter(taskModel.getStartAt());
+        Boolean endIsBefore = currentDate.isAfter(taskModel.getEndDate());
+        Boolean endIsBeforeOfStart = taskModel.getStartAt().isAfter(taskModel.getEndDate());
+
+        if (startIsBefore || endIsBefore) {
+            return ResponseEntity.badRequest().body(
+                    "You can't attribute a past date.");
+        }
+
+        if (endIsBeforeOfStart) {
+            return ResponseEntity.badRequest().body(
+                    "You can't finish a task with a past date.");
+        }
+
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.ok().body(task);
     }
 }
